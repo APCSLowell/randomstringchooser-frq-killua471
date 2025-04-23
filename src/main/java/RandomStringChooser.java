5215659848342652f266531a7e902e549a3971b9
Justin <justinlei471@gmail.com>
2025-04-23 09:55:20 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -4,14 +4,14 @@ public class RandomStringChooser
   /* to be implemented in part (a) */
   private String[] main;
   private int available;
-  public RandomStringChooser(string[] a){
+  public RandomStringChooser(String[] a){
     for(int i =0; i<a.length;i++){
       main[i]=a[i];
     }
     available=a.length;
   }
   public String getNext(){
-    string temp;
+    String temp;
     int randomN=(int)(Math.random()*main.length);
     if(available==0){
       return "NONE";

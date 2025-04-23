c536fa9c8934f739c94d67e00dfe072f4767e5f5
Justin <justinlei471@gmail.com>
2025-04-23 09:53:09 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,29 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  
+  private String[] main;
+  private int available;
+  public RandomStringChooser(string[] a){
+    for(int i =0; i<a.length;i++){
+      main[i]=a[i];
+    }
+    available=a.length;
+  }
+  public String getNext(){
+    string temp;
+    int randomN=(int)(Math.random()*main.length);
+    if(available==0){
+      return "NONE";
+    }
+    if(!main[randomN].equals("unavailable")){
+      temp = main[randomN];
+      main[randomN]="unavailable";
+      available--;
+      return temp;
+    }else{
+      getNext();
+    }
+  }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 }
